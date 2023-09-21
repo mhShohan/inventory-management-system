173179619b07871f4f2537d10dcd4123bc5ea750
mhshohan1 <mehdihasanshohan25@gmail.com>
2023-09-22 00:53:01 +0600

    API - login & logout

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler');
 const generateToken = require('../utils/generateToken');
+const bcrypt = require('bcrypt');
 
 //project import 
 const User = require('../model/User');
@@ -48,6 +49,53 @@ handler.registerUser = asyncHandler(async (req, res) => {
     }
 });
 
+handler.login = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
 
+    if (!email || !password) {
+        res.status(400);
+        throw new Error('All fields are required!');
+    }
+
+    //check existing User
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        res.status(404);
+        throw new Error('User not found!');
+    }
+
+    const matchedPassword = await bcrypt.compare(password, user.password);
+
+    if (user && matchedPassword) {
+        const token = generateToken({ _id: user._id, email: user.email, name: user.name });
+
+        //send HTTP only cookie
+        res.cookie('token', token, {
+            path: '/',
+            httpOnly: true,
+            expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+            sameSite: 'none',
+            secure: true
+        });
+
+        return res.status(200).json({ email: user.email, name: user.name, token });
+    } else {
+        res.status(400);
+        throw new Error('Invalid credentials');
+    }
+});
+
+handler.logout = asyncHandler(async (req, res) => {
+    res.cookie('token', '', {
+        path: '/',
+        httpOnly: true,
+        expires: new Date(0),
+        sameSite: 'none',
+        secure: true
+    });
+
+    return res.status(200).json({ message: 'Logout successfully!' });
+});
 
 module.exports = handler;
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userController = require('../controller/userController');
 
-router.post('/', userController.registerUser);
+router.post('/register', userController.registerUser);
+router.post('/login', userController.login);
+router.get('/logout', userController.logout);
 
 module.exports = router;
