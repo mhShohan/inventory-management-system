bd36704b816d39695d9a2029d7d6cae0994805a1
mhshohan1 <mehdihasanshohan25@gmail.com>
2024-01-28 14:42:06 +0600

    refactor to new project

--- server/routes/index.js
+++
@@ -1,5 +0,0 @@
-const router = require('express').Router();
-
-router.use('/users', require('./userRoutes'));
-
-module.exports = router;
--- server/utils/generateToken.js
+++
@@ -1,7 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const generateToken = (payload) => {
-    return jwt.sign(payload, process.env.JWT, { expiresIn: '7d' });
-};
-
-module.exports = generateToken;
