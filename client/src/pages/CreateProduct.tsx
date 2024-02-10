34d4cc7f743212577b78c2fa967c05794a3fa73a
mhshohan1 <mehdihasanshohan25@gmail.com>
2024-02-10 18:04:30 +0600

    [client] - create product page refactor

--- client/src/pages/CreateProduct.tsx
+++ client/src/pages/CreateProduct.tsx
@@ -24,6 +24,10 @@ const CreateProduct = () => {
     payload.price = Number(data.price);
     payload.stock = Number(data.stock);
 
+    if (payload.size === '') {
+      delete payload.size;
+    }
+
     try {
       const res = await createNewProduct(payload).unwrap();
       if (res.statusCode === 201) {
@@ -31,129 +35,210 @@ const CreateProduct = () => {
         reset();
       }
     } catch (error: any) {
+      console.log(error);
+
       toastMessage({ icon: 'error', text: error.data.message });
     }
   };
 
   return (
-    <Flex justify='center' align='center'>
-      <Flex
-        vertical
+    <Row
+      gutter={30}
+      style={{
+        height: 'calc(100vh - 6rem)',
+        overflow: 'auto',
+      }}
+    >
+      <Col
+        xs={{ span: 24 }}
+        lg={{ span: 14 }}
         style={{
-          width: '500px',
-          margin: '1rem 0',
-          padding: '1rem 2rem',
-          border: '1px solid #164863',
-          borderRadius: '.6rem',
+          display: 'flex',
         }}
       >
-        <h1
+        <Flex
+          vertical
           style={{
-            marginBottom: '.8rem',
-            fontWeight: '900',
-            textAlign: 'center',
-            textTransform: 'uppercase',
+            width: '100%',
+            padding: '1rem 2rem',
+            border: '1px solid #164863',
+            borderRadius: '.6rem',
           }}
         >
-          Add New Product
-        </h1>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <CustomInput
-            name='name'
-            errors={errors}
-            label='Name'
-            register={register}
-            required={true}
-          />
-          <CustomInput
-            errors={errors}
-            label='Price'
-            type='number'
-            name='price'
-            register={register}
-            required={true}
-          />
-          <CustomInput
-            errors={errors}
-            label='Stock'
-            type='number'
-            name='stock'
-            register={register}
-            required={true}
-          />
+          <h1
+            style={{
+              marginBottom: '.8rem',
+              fontWeight: '900',
+              textAlign: 'center',
+              textTransform: 'uppercase',
+            }}
+          >
+            Add New Product
+          </h1>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <CustomInput
+              name='name'
+              errors={errors}
+              label='Name'
+              register={register}
+              required={true}
+            />
+            <CustomInput
+              errors={errors}
+              label='Price'
+              type='number'
+              name='price'
+              register={register}
+              required={true}
+            />
+            <CustomInput
+              errors={errors}
+              label='Stock'
+              type='number'
+              name='stock'
+              register={register}
+              required={true}
+            />
 
-          <Row>
-            <Col xs={{ span: 23 }} lg={{ span: 6 }}>
-              <label htmlFor='Size' className='label'>
-                Category
-              </label>
-            </Col>
-            <Col xs={{ span: 23 }} lg={{ span: 18 }}>
-              <select
-                {...register('category', { required: true })}
-                className={`input-field ${errors['category'] ? 'input-field-error' : ''}`}
+            <Row>
+              <Col xs={{ span: 23 }} lg={{ span: 6 }}>
+                <label htmlFor='Size' className='label'>
+                  Category
+                </label>
+              </Col>
+              <Col xs={{ span: 23 }} lg={{ span: 18 }}>
+                <select
+                  {...register('category', { required: true })}
+                  className={`input-field ${errors['category'] ? 'input-field-error' : ''}`}
+                >
+                  <option value=''>Select Category*</option>
+                  {categories?.data.map((item: ICategory) => (
+                    <option value={item._id}>{item.name}</option>
+                  ))}
+                </select>
+              </Col>
+            </Row>
+
+            <Row>
+              <Col xs={{ span: 23 }} lg={{ span: 6 }}>
+                <label htmlFor='Size' className='label'>
+                  Brand
+                </label>
+              </Col>
+              <Col xs={{ span: 23 }} lg={{ span: 18 }}>
+                <select
+                  {...register('brand', { required: true })}
+                  className={`input-field ${errors['brand'] ? 'input-field-error' : ''}`}
+                >
+                  <option value=''>Select brand</option>
+                  {brands?.data.map((item: ICategory) => (
+                    <option value={item._id}>{item.name}</option>
+                  ))}
+                </select>
+              </Col>
+            </Row>
+
+            <CustomInput label='Variant' name='variant' register={register} />
+
+            <CustomInput label='Description' name='description' register={register} />
+
+            <Row>
+              <Col xs={{ span: 23 }} lg={{ span: 6 }}>
+                <label htmlFor='Size' className='label'>
+                  Size
+                </label>
+              </Col>
+              <Col xs={{ span: 23 }} lg={{ span: 18 }}>
+                <select className={`input-field`}>
+                  <option value=''>Select Product Size</option>
+                  <option value='SMALL'>Small</option>
+                  <option value='MEDIUM'>Medium</option>
+                  <option value='LARGE'>Large</option>
+                </select>
+              </Col>
+            </Row>
+            <Flex justify='center'>
+              <Button
+                htmlType='submit'
+                type='primary'
+                style={{ textTransform: 'uppercase', fontWeight: 'bold' }}
               >
-                <option value=''>Select Category*</option>
-                {categories?.data.map((item: ICategory) => (
-                  <option value={item._id}>{item.name}</option>
-                ))}
-              </select>
-            </Col>
-          </Row>
-
-          <Row>
-            <Col xs={{ span: 23 }} lg={{ span: 6 }}>
-              <label htmlFor='Size' className='label'>
-                Brand
-              </label>
-            </Col>
-            <Col xs={{ span: 23 }} lg={{ span: 18 }}>
-              <select
-                {...register('brand')}
-                className={`input-field ${errors['brand'] ? 'input-field-error' : ''}`}
-              >
-                <option value=''>Select brand</option>
-                {brands?.data.map((item: ICategory) => (
-                  <option value={item._id}>{item.name}</option>
-                ))}
-              </select>
-            </Col>
-          </Row>
-
-          <CustomInput label='Variant' name='variant' register={register} />
-
-          <CustomInput label='Description' name='description' register={register} />
-
-          <Row>
-            <Col xs={{ span: 23 }} lg={{ span: 6 }}>
-              <label htmlFor='Size' className='label'>
-                Size
-              </label>
-            </Col>
-            <Col xs={{ span: 23 }} lg={{ span: 18 }}>
-              <select
-                {...register('size', { required: true })}
-                className={`input-field ${errors['size'] ? 'input-field-error' : ''}`}
-              >
-                <option value=''>Select Product Size</option>
-                <option value='SMALL'>Small</option>
-                <option value='MEDIUM'>Medium</option>
-                <option value='LARGE'>Large</option>
-              </select>
-            </Col>
-          </Row>
-          <Flex justify='center'>
+                Create Product
+              </Button>
+            </Flex>
+          </form>
+        </Flex>
+      </Col>
+      <Col xs={{ span: 24 }} lg={{ span: 10 }}>
+        <Flex
+          vertical
+          style={{
+            width: '100%',
+            height: '100%',
+            padding: '1rem 2rem',
+            border: '1px solid #164863',
+            borderRadius: '.6rem',
+            justifyContent: 'space-around',
+          }}
+        >
+          <Flex
+            vertical
+            style={{
+              padding: '1rem 2rem',
+              border: '1px solid #b6cbd7',
+              borderRadius: '.6rem',
+              marginBottom: '1rem',
+            }}
+          >
+            <h3
+              style={{
+                textAlign: 'center',
+                marginBottom: '.6rem',
+                fontWeight: '900',
+                textTransform: 'uppercase',
+              }}
+            >
+              Create New Category
+            </h3>
+            <input type='text' className='input-field' placeholder='Category Name' />
             <Button
               htmlType='submit'
               type='primary'
               style={{ textTransform: 'uppercase', fontWeight: 'bold' }}
             >
-              Create Product
+              Create Category
             </Button>
           </Flex>
-        </form>
-      </Flex>
-    </Flex>
+          <Flex
+            vertical
+            style={{
+              padding: '1rem 2rem',
+              border: '1px solid #b6cbd7',
+              borderRadius: '.6rem',
+            }}
+          >
+            <h3
+              style={{
+                textAlign: 'center',
+                marginBottom: '.6rem',
+                fontWeight: '900',
+                textTransform: 'uppercase',
+              }}
+            >
+              Create New Brand
+            </h3>
+            <input type='text' className='input-field' placeholder='Brand Name' />
+            <Button
+              htmlType='submit'
+              type='primary'
+              style={{ textTransform: 'uppercase', fontWeight: 'bold' }}
+            >
+              Create Brand
+            </Button>
+          </Flex>
+        </Flex>
+      </Col>
+    </Row>
   );
 };
 
