e68ac0958e8de840c65594ba867332d4a1875091
mhshohan1 <mehdihasanshohan25@gmail.com>
2024-02-24 15:57:38 +0600

    update profile page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Toaster
           duration={2000}
           toastOptions={{
-            style: { background: 'rgba(22, 72, 99, 0.7)' },
+            style: { background: 'rgba(22, 72, 99, 0.7)', color: '#fff', fontWeight: 900 },
           }}
         />
       </PersistGate>
--- client/src/pages/ProfilePage.tsx
+++ client/src/pages/ProfilePage.tsx
@@ -1,4 +1,4 @@
-import { Flex } from 'antd';
+import { Flex, Tag } from 'antd';
 import Loader from '../components/Loader';
 import { useGetSelfProfileQuery } from '../redux/features/authApi';
 
@@ -7,12 +7,43 @@ const ProfilePage = () => {
 
   if (isLoading) return <Loader />;
 
+  console.log(data);
+
   return (
     <Flex justify='center' align='center' style={{ minHeight: 'calc(100vh - 10rem)' }}>
-      <Flex vertical align='center'>
-        <h1 style={{ fontSize: '3rem', textTransform: 'uppercase' }}>Welcome</h1>
-        <h2 style={{ fontSize: '2rem' }}>Name: {data?.data?.name}</h2>
-        <h2 style={{ fontSize: '1.5rem' }}> {data?.data?.email}</h2>
+      <Flex
+        align='center'
+        style={{
+          minWidth: '400px',
+          border: '1px solid gray',
+          padding: '1rem',
+          borderRadius: '1rem',
+          gap: '1rem',
+        }}
+      >
+        <Flex
+          justify='center'
+          align='center'
+          style={{
+            width: '80px',
+            height: '80px',
+            backgroundColor: 'lightblue',
+            borderRadius: '50%',
+          }}
+        >
+          <h1>
+            {data?.data?.name
+              .split(' ')
+              .map((item: string) => item.charAt(0).toUpperCase())
+              .join('')}
+          </h1>
+        </Flex>
+        <div>
+          <h1 style={{ lineHeight: '1.2rem' }}>Name: {data?.data?.name}</h1>
+          <h3> {data?.data?.email}</h3>
+          {data?.data.status === 'BLOCK' && <Tag color='red'>{data?.data.status}</Tag>}
+          {data?.data.status === 'ACTIVE' && <Tag color='green'>{data?.data.status}</Tag>}
+        </div>
       </Flex>
     </Flex>
   );
