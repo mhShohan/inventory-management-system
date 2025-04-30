31caee2654a40ae1e730ae7cfe5fe0b6ff2e1df7
mhShohan <mehdihasanshohan25@gmail.com>
2025-04-30 17:44:15 +0600

    theme

--- nextjs-client/src/components/ToggleSwitch.tsx
+++ nextjs-client/src/components/ToggleSwitch.tsx
@@ -1,41 +1,35 @@
-import { useState } from 'react';
+'use client';
+
+import { useEffect, useState } from 'react';
 import { Sun, Moon } from 'lucide-react';
+import { useTheme } from 'next-themes';
 
 const ThemeSwitch = () => {
-  // State to track whether the theme is dark
-  const [isDark, setIsDark] = useState(false);
+  const { setTheme, theme } = useTheme();
+  const [isDark, setIsDark] = useState(theme === 'dark');
 
-  // Function to toggle theme
-  const toggleTheme = () => {
-    const newTheme = !isDark ? 'dark' : 'light';
-    setIsDark(!isDark);
-  };
+  useEffect(() => {
+    const newTheme = isDark ? 'dark' : 'light';
+    setTheme(newTheme);
+  }, [isDark, setTheme]);
 
   return (
-    <div className='flex items-center justify-center'>
+    <div className='flex items-center justify-center cursor-pointer'>
       <button
-        onClick={toggleTheme}
-        className='relative inline-flex h-8 w-16 items-center rounded-full transition-colors focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'
-        style={{
-          backgroundColor: isDark ? '#3b82f6' : '#e5e7eb',
-        }}
+        onClick={() => setIsDark((p) => !p)}
+        className={`relative inline-flex h-8 w-16 items-center rounded-full transition-colors focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 ${
+          isDark ? 'bg-gray-400' : 'bg-gray-700'
+        }`}
       >
-        <span className='sr-only'>Toggle theme</span>
-
-        {/* Toggle circle */}
-        <span
+        <div
           className={`
             flex h-6 w-6 transform items-center justify-center rounded-full bg-white transition-transform
             ${isDark ? 'translate-x-9' : 'translate-x-1'}
           `}
         >
           {/* Icons */}
-          {isDark ? (
-            <Moon size={14} className='text-blue-700' />
-          ) : (
-            <Sun size={14} className='text-yellow-500' />
-          )}
-        </span>
+          {isDark ? <Moon size={14} /> : <Sun size={14} />}
+        </div>
       </button>
     </div>
   );
